94f04d4ad3e81bbc02faa2464673a4fd6c7caeb9
Maxim Mironyuk (serieznyi) <serieznyi@gmail.com>
2021-11-07 21:24:42 +0300

    add files for configure compatibility

--- src/settings-updates.lua
+++ src/settings-updates.lua
@@ -1,12 +1,5 @@
-require ("prototypes.technology")
-require ("prototypes.recipe")
-require ("prototypes.item")
-require ("prototypes.entity")
-
-automated_train_depot = {}
-
 ---------------------------------------------------------------------------
 -- -- -- Compatibility initialization
 ---------------------------------------------------------------------------
 
-require('compatibility.1-data.index')
+require('compatibility.0-settings-updates.index')
