70bfbbf9216756436063818f4befb8de20043421
Maxim Mironyuk (serieznyi) <serieznyi@gmail.com>
2021-11-07 16:52:51 +0300

    add first setting

--- settings.lua
+++ settings.lua
@@ -1,3 +1,10 @@
 data:extend({
-
+    {
+        type = "string-setting",
+        name = "atd-console-level",
+        --order = "ad",
+        setting_type = "runtime-global",
+        default_value = "2",
+        allowed_values = {"0", "1", "2", "3"}
+    },
 })
