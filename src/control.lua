c684b4f6673fe40711f79593f246e971ac4653b4
Maxim Mironyuk (serieznyi) <serieznyi@gmail.com>
2021-12-12 17:24:01 +0300

    register events in control

--- src/control.lua
+++ src/control.lua
@@ -1,5 +1,9 @@
 automated_train_depot = require("extra.logic.init_modification_state")
 
+local event = require("__flib__.event")
+local gui = require("__flib__.gui")
+local default_controller = require("extra.logic.controllers.default_controller")
+
 -- TODO
 --script.on_nth_tick(, function()
 --
@@ -31,4 +35,27 @@ script.on_load(function()
     -- TODO
 end)
 
-require('extra.logic.register_event_handlers')
+event.register(
+        {
+            defines.events.on_built_entity,
+            defines.events.on_robot_built_entity,
+        },
+        function(e) default_controller:on_build_entity(e) end,
+        {{ filter="name", name= automated_train_depot.constants.entity_names.depot_building }}
+)
+
+event.register({
+    defines.events.on_player_mined_entity,
+    defines.events.on_robot_mined_entity,
+    defines.events.on_entity_died,
+}, function(e)
+    default_controller:on_deconstruct_entity(e)
+end)
+
+event.register(defines.events.on_runtime_mod_setting_changed, function(e)
+    default_controller:on_runtime_mod_setting_changed(e)
+end)
+
+event.register(defines.events.on_gui_opened, function(e)
+    default_controller:on_gui_opened(e)
+end)
