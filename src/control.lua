4b6cad16eadda0ffece4fd404076031b582c7ce2
Maxim Mironyuk (serieznyi) <serieznyi@gmail.com>
2021-12-12 21:41:25 +0300

    register events in control

--- src/control.lua
+++ src/control.lua
@@ -1,13 +1,16 @@
+local event = require("__flib__.event")
+local dictionary = require("__flib__.dictionary")
+local gui = require("__flib__.gui")
+local migration = require("__flib__.migration")
+local on_tick_n = require("__flib__.on-tick-n")
+
 automated_train_depot = require("extra.logic.init_modification_state")
 
-local event = require("__flib__.event")
-local gui = require("__flib__.gui")
 local default_controller = require("extra.logic.controllers.default_controller")
 
--- TODO
---script.on_nth_tick(, function()
---
---end)
+---------------------------------------------------------------------------
+-- -- -- Main events
+---------------------------------------------------------------------------
 
 -- Game version changed
 -- Any mod version changed
@@ -16,24 +19,35 @@ local default_controller = require("extra.logic.controllers.default_controller")
 -- Any mod prototypes changed
 -- Any mod settings changed
 script.on_configuration_changed(function()
--- TODO
+    if migration.on_config_changed(e, migrations) then
+        dictionary.init()
+
+        -- TODO
+    end
 end)
 
 -- Save file created
 -- Loaded save file what don`t contain mod
 -- Can write in `global` and read `game`
-script.on_init(function()
+event.on_init(function()
+    dictionary.init()
+    on_tick_n.init()
+
     -- Init local vars
-    -- TODO
 end)
 
 -- Loaded save file what contains mod
 -- Cant write in global
-script.on_load(function()
+event.on_load(function()
+    dictionary.load()
     -- Restore local vars from `global`
     -- Re-register event handlers
     -- TODO
 end)
+
+---------------------------------------------------------------------------
+-- -- -- Mod events
+---------------------------------------------------------------------------
 
 event.register(
         {
@@ -44,13 +58,14 @@ event.register(
         {{ filter="name", name= automated_train_depot.constants.entity_names.depot_building }}
 )
 
-event.register({
-    defines.events.on_player_mined_entity,
-    defines.events.on_robot_mined_entity,
-    defines.events.on_entity_died,
-}, function(e)
-    default_controller:on_deconstruct_entity(e)
-end)
+event.register(
+        {
+            defines.events.on_player_mined_entity,
+            defines.events.on_robot_mined_entity,
+            defines.events.on_entity_died,
+        },
+        function(e) default_controller:on_deconstruct_entity(e)end
+)
 
 event.register(defines.events.on_runtime_mod_setting_changed, function(e)
     default_controller:on_runtime_mod_setting_changed(e)
