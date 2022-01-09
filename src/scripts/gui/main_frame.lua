833d2a49d73c2aadbfc08cf2008ea5d25e07b2e0
Maxim Mironyuk (serieznyi) <serieznyi@gmail.com>
2022-01-09 16:02:49 +0300

    save

--- src/scripts/gui/main_frame.lua
+++ src/scripts/gui/main_frame.lua
@@ -2,14 +2,128 @@ local flib_gui = require("__flib__.gui")
 
 local frame = {}
 
-local NAME = "main_frame"
+local FRAME_NAME = "main_frame"
+
+local ACTION = {
+    CLOSE = "close",
+}
+
+---@return table
+local function gui_build_structure_frame()
+    return {
+        type = "frame",
+        name = FRAME_NAME,
+        direction = "vertical",
+        ref  =  {"window"},
+        visible = false,
+        style_mods = {
+            natural_width = 1200,
+            natural_height = 400,
+        },
+        children = {
+            -- Titlebar
+            {
+                type = "flow",
+                style = "flib_titlebar_flow",
+                ref = {"titlebar_flow"},
+                children = {
+                    {
+                        type = "label",
+                        style = "frame_title",
+                        caption = {"gui-name.automated-train-depot-main-frame"},
+                        ignored_by_interaction = true
+                    },
+                    {
+                        type = "empty-widget",
+                        style = "flib_titlebar_drag_handle",
+                        ignored_by_interaction = true
+                    },
+                    {
+                        type = "sprite-button",
+                        style = "frame_action_button",
+                        sprite = "utility/close_white",
+                        hovered_sprite = "utility/close_black",
+                        clicked_sprite = "utility/close_black",
+                        ref = {"titlebar", "close_button"},
+                        actions = {
+                            on_click = {gui = FRAME_NAME, action = ACTION.CLOSE}
+                        }
+                    }
+                }
+            },
+            -- Content
+            {
+                type = "frame",
+                style = "inside_shallow_frame_with_padding",
+                direction = "horizontal",
+                children = {
+                    {
+                        type = "button",
+                        caption = "add",
+                        actions = {
+                            on_click = { gui = "add_group_frame", action = "open" },
+                        },
+                    },
+                    {
+                        type = "frame",
+                        name = "groups_container",
+                        style_mods = {
+                            maximal_width = 400, -- todo use var
+                        },
+                        --style = "inside_shallow_frame_with_padding",
+                        direction = "vertical",
+                        children = {
+                            {
+                                type = "list-box",
+                                items = {
+                                    "asd1",
+                                    "asd2",
+                                }
+                            }
+                        }
+                    },
+                    {
+                        type = "frame",
+                        name = "group_view",
+                        --style = "inside_shallow_frame_with_padding",
+                        direction = "vertical",
+                        children = {
+                            {
+                                type = "tabbed-pane",
+                                children = {
+                                    {
+                                        tab = {
+                                            type = "tab",
+                                            caption = { "gui-name.automated-train-depot-group-view" },
+                                            ref = { "atd-concrete-group-view-tab" },
+                                            actions = {
+                                                --on_click = { gui = "main", action = "change_tab", tab = "trains" },
+                                            },
+                                        },
+                                        content = {
+                                            type = "frame",
+                                            --style = "ltnm_main_content_frame",
+                                            direction = "vertical",
+                                            ref = {"atd-concrete-group-view-content"},
+
+                                        }
+                                    }
+                                },
+                            }
+                        },
+                    }
+                }
+            }
+        }
+    }
+end
 
 function frame.get_name()
-    return NAME
+    return FRAME_NAME
 end
 
 function frame.init()
-    global.gui[NAME] = {}
+    global.gui[FRAME_NAME] = {}
 end
 
 ---@param player LuaPlayer
@@ -21,13 +135,13 @@ end
 ---@param player LuaPlayer
 ---@param entity LuaEntity
 function frame.open(player, entity)
-    if global.gui[NAME][player.index] == nil then
+    if global.gui[FRAME_NAME][player.index] == nil then
         frame.create(player, entity)
     else
         frame.update(player, entity)
     end
 
-    local gui = global.gui[NAME][player.index]
+    local gui = global.gui[FRAME_NAME][player.index]
 
     gui.refs.window.bring_to_front()
     gui.refs.window.visible = true
@@ -38,119 +152,12 @@ end
 ---@param player LuaPlayer
 ---@param entity LuaEntity
 function frame.create(player, entity)
-    local refs = flib_gui.build(player.gui.screen, {
-        {
-            type = "frame",
-            name = "depot_main_frame",
-            direction = "vertical",
-            ref  =  {"window"},
-            visible = false,
-            style_mods = {
-                natural_width = 1200,
-                natural_height = 400,
-            },
-            children = {
-                -- Titlebar
-                {
-                    type = "flow",
-                    style = "flib_titlebar_flow",
-                    ref = {"titlebar_flow"},
-                    children = {
-                        {
-                            type = "label",
-                            style = "frame_title",
-                            caption = {"gui-name.automated-train-depot-main-frame"},
-                            ignored_by_interaction = true
-                        },
-                        {
-                            type = "empty-widget",
-                            style = "flib_titlebar_drag_handle",
-                            ignored_by_interaction = true
-                        },
-                        {
-                            type = "sprite-button",
-                            style = "frame_action_button",
-                            sprite = "utility/close_white",
-                            hovered_sprite = "utility/close_black",
-                            clicked_sprite = "utility/close_black",
-                            ref = {"titlebar", "close_button"},
-                            actions = {
-                                on_click = {gui = "main_frame", action = "close"}
-                            }
-                        }
-                    }
-                },
-                -- Content
-                {
-                    type = "frame",
-                    style = "inside_shallow_frame_with_padding",
-                    direction = "horizontal",
-                    children = {
-                        {
-                            type = "button",
-                            caption = "add",
-                            actions = {
-                                on_click = { gui = "add_group_frame", action = "open" },
-                            },
-                        },
-                        {
-                            type = "frame",
-                            name = "groups_container",
-                            style_mods = {
-                                maximal_width = 400, -- todo use var
-                            },
-                            --style = "inside_shallow_frame_with_padding",
-                            direction = "vertical",
-                            children = {
-                                {
-                                    type = "list-box",
-                                    items = {
-                                        "asd1",
-                                        "asd2",
-                                    }
-                                }
-                            }
-                        },
-                        {
-                            type = "frame",
-                            name = "group_view",
-                            --style = "inside_shallow_frame_with_padding",
-                            direction = "vertical",
-                            children = {
-                                {
-                                    type = "tabbed-pane",
-                                    children = {
-                                        {
-                                            tab = {
-                                                type = "tab",
-                                                caption = { "gui-name.automated-train-depot-group-view" },
-                                                ref = { "atd-concrete-group-view-tab" },
-                                                actions = {
-                                                    --on_click = { gui = "main", action = "change_tab", tab = "trains" },
-                                                },
-                                            },
-                                            content = {
-                                                type = "frame",
-                                                --style = "ltnm_main_content_frame",
-                                                direction = "vertical",
-                                                ref = {"atd-concrete-group-view-content"},
-
-                                            }
-                                        }
-                                    },
-                                }
-                            },
-                        }
-                    }
-                }
-            }
-        }
-    })
+    local refs = flib_gui.build(player.gui.screen, {gui_build_structure_frame()})
 
     refs.window.force_auto_center()
     refs.titlebar_flow.drag_target = refs.window
 
-    global.gui[NAME][player.index] = {
+    global.gui[FRAME_NAME][player.index] = {
         refs = refs,
         state = {
             entity = entity,
@@ -160,10 +167,10 @@ function frame.create(player, entity)
 end
 
 function frame.destroy(player)
-    local gui_data = global.gui[NAME][player.index]
+    local gui_data = global.gui[FRAME_NAME][player.index]
 
     if gui_data then
-        global.gui[NAME][player.index] = nil
+        global.gui[FRAME_NAME][player.index] = nil
         gui_data.refs.window.destroy()
     end
 end
@@ -171,14 +178,14 @@ end
 ---@param action table
 ---@param event EventData
 function frame.dispatch(action, event)
-    if action.action == "close" then
+    if action.action == ACTION.CLOSE then
         frame.close(event)
     end
 end
 
 function frame.close(event)
     local player = game.get_player(event.player_index)
-    local gui = global.gui[NAME][player.index]
+    local gui = global.gui[FRAME_NAME][player.index]
 
     gui.refs.window.visible = false
     gui.state.visible = false
