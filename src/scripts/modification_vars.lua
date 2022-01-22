9258aeb8bda31f807fb4424daefd8cd28e89670f
Maxim Mironyuk (serieznyi) <serieznyi@gmail.com>
2022-01-22 19:41:29 +0300

    add mod storage

--- src/scripts/modification_vars.lua
+++ src/scripts/modification_vars.lua
@@ -6,6 +6,8 @@ mod.util.table = require("scripts.util.table")
 mod.util.console = require("scripts.console")
 mod.util.logger = require("scripts.logger")
 
+-- Save module runtime vars
+mod.storage = {}
 mod.depots = {}
 
 return mod
