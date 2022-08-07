bb624345553d389921d42ddca6f90d988476250a
Maxim Mironyuk (serieznyi) <serieznyi@gmail.com>
2022-08-07 13:47:55 +0300

    gui -> image

--- src/scripts/util/image.lua
+++ src/scripts/util/image.lua
@@ -1,7 +1,7 @@
-local gui = {}
+local image = {}
 
 ---@param item_name string
-function gui.image_path_for_item(item_name)
+function image.image_path_for_item(item_name)
     local icon_path = "item/" .. item_name
 
     if not game.is_valid_sprite_path(icon_path) then
@@ -12,10 +12,10 @@ function gui.image_path_for_item(item_name)
 end
 
 ---@param item_name string
-function gui.image_for_item(item_name)
-    local icon_path = gui.image_path_for_item(item_name)
+function image.image_for_item(item_name)
+    local icon_path = image.image_path_for_item(item_name)
 
     return "[img=" .. icon_path .. "]"
 end
 
-return gui
+return image
