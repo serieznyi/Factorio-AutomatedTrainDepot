05bd3d0bcb53532a911ef639c48e75e2cabbd73e
Maxim Mironyuk <serieznyi@gmail.com>
2022-10-23 13:38:28 +0300

    train_task_service -> train_template_service

--- src/scripts/lib/train_template_service.lua
+++ src/scripts/lib/train_template_service.lua
@@ -1,56 +1,53 @@
-local flib_train = require("__flib__.train")
-
 local logger = require("scripts.lib.logger")
-local Train = require("scripts.lib.domain.entity.Train")
 local persistence_storage = require("scripts.persistence.persistence_storage")
 
-local TrainTaskService = {}
+local TrainTemplateService = {}
 
-function TrainTaskService.init()
+function TrainTemplateService.init()
 end
 
-function TrainTaskService.load()
+function TrainTemplateService.load()
 end
 
 ---@param train_template_id uint
 ---@return void
-function TrainTaskService.enable_train_template(train_template_id)
+function TrainTemplateService.enable_train_template(train_template_id)
     local train_template = persistence_storage.find_train_template_by_id(train_template_id)
 
     train_template.enabled = true
 
     train_template = persistence_storage.add_train_template(train_template)
 
-    TrainTaskService._raise_train_template_changed_event(train_template)
+    TrainTemplateService._raise_train_template_changed_event(train_template)
 end
 
 ---@param train_template_id uint
 ---@return void
-function TrainTaskService.disable_train_template(train_template_id)
+function TrainTemplateService.disable_train_template(train_template_id)
     local train_template = persistence_storage.find_train_template_by_id(train_template_id)
     train_template.enabled = false
 
     train_template = persistence_storage.add_train_template(train_template)
 
-    TrainTaskService._raise_train_template_changed_event(train_template)
+    TrainTemplateService._raise_train_template_changed_event(train_template)
 end
 
 ---@param train_template_id uint
 ---@param count int
 ---@return void
-function TrainTaskService.change_trains_quantity(train_template_id, count)
+function TrainTemplateService.change_trains_quantity(train_template_id, count)
     local train_template = persistence_storage.find_train_template_by_id(train_template_id)
 
     train_template:change_trains_quantity(count)
     persistence_storage.add_train_template(train_template)
 
-    TrainTaskService._raise_train_template_changed_event(train_template)
+    TrainTemplateService._raise_train_template_changed_event(train_template)
 
     return train_template
 end
 
 ---@param train_template scripts.lib.domain.entity.template.TrainTemplate
-function TrainTaskService._raise_train_template_changed_event(train_template)
+function TrainTemplateService._raise_train_template_changed_event(train_template)
     logger.debug(
             "Changed train template (`1`)",
             { train_template.id },
@@ -69,4 +66,4 @@ function TrainTaskService._raise_train_template_changed_event(train_template)
 
 end
 
-return TrainTaskService
+return TrainTemplateService
