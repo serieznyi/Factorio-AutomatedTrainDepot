e85f404994bc301a06e0e1d14f8119148691373c
Maxim Mironyuk <serieznyi@gmail.com>
2022-10-29 19:06:19 +0300

    Add suppressing logging part of events in EventDispatcher

--- src/scripts/lib/event/EventDispatcher.lua
+++ src/scripts/lib/event/EventDispatcher.lua
@@ -1,10 +1,14 @@
 local logger = require("scripts.lib.logger")
+local util_table = require("scripts.util.table")
 
 local EventDispatcher = {
     handlers = {},
 }
 
 EventDispatcher.LOGGING_ENABLED = false
+EventDispatcher.LOGGING_SUPPRESS_EVENT_NAMES = {
+    defines.events.on_train_changed_state,
+}
 
 ---@param match function
 ---@param handler function
@@ -55,20 +59,20 @@ end
 function EventDispatcher.dispatch(event)
     local processed = false
 
-    if EventDispatcher.LOGGING_ENABLED then
+    if EventDispatcher.LOGGING_ENABLED and not EventDispatcher._is_suppressed(event.id) then
         logger.debug("Taken event `{1}`", { event:full_name() }, "EventDispatcher")
     end
 
-    for _, source_handlers in pairs(EventDispatcher.handlers) do
+    for source_name, source_handlers in pairs(EventDispatcher.handlers) do
         for _, h in ipairs(source_handlers) do
             if h.match(event) and h.handler(event) then
                 processed = true
 
-                if EventDispatcher.LOGGING_ENABLED then
+                if EventDispatcher.LOGGING_ENABLED and not EventDispatcher._is_suppressed(event.id) then
                     logger.debug(
-                            "Handled event {1} for {2}",
-                            { tostring(event), (h.handler_source and h.handler_source or '?') },
-                            "EventDispatcher"
+                        "Handled event {1} for {2}",
+                        { tostring(event), source_name },
+                        "EventDispatcher"
                     )
                 end
             end
@@ -76,6 +80,11 @@ function EventDispatcher.dispatch(event)
     end
 
     return processed
+end
+
+---@param event_name uint
+function EventDispatcher._is_suppressed(event_name)
+    return util_table.find(EventDispatcher.LOGGING_SUPPRESS_EVENT_NAMES, event_name) ~= nil
 end
 
 return EventDispatcher
